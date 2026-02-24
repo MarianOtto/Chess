b27dd1372e7601c4139acf26b643c3fb73dbe459
Marian <marian.otto@gmx.net>
2026-02-24 21:24:09 +0100

    Add Given When Then structure and addFigure test

--- test/test_square.cpp
+++ test/test_square.cpp
@@ -1,28 +1,107 @@
 #include "../include/catch.hpp"
 #include "../include/square.h"
+#include "../include/figure.h"
+#include "../include/board.h"
 #include <string>
 
 TEST_CASE("Create Square", "[square]"){
-    Square square(3); //Create square with idx 35
+    Board board;
+    SECTION("Square at 0"){
+        Square square(0, board); //Create square with idx 35
 
-    CHECK(square.index() == 3);
-    CHECK(square.bit() == 0b1000);
-    CHECK(square.notation() == "A4");
+        CHECK(square.index() == 0);
+        CHECK(square.bit() == 0b1);
+        CHECK(square.notation() == "A1");
+    }
+    SECTION("Square at 63"){
+        Square square(63, board); //Create square with idx 35
+
+        CHECK(square.index() == 63);
+        CHECK(square.bit() == 0x8000000000000000);
+        CHECK(square.notation() == "H8");
+    }
 }
 
 TEST_CASE("Create Wrong Squares", "[square]"){
-    CHECK_THROWS(Square(65));
-    CHECK_THROWS(Square(-1));
-    CHECK_THROWS(Square::fromNotation(("longstring")));
-    CHECK_THROWS(Square::fromNotation(("a9")));
-    CHECK_THROWS(Square::fromNotation(("i1")));
-    CHECK_THROWS(Square::fromNotation(("I1")));
+    Board board;
+    GIVEN("Wrong parameters for Square creation"){
+        WHEN("You create the Square with an index too big"){
+            THEN("The Square should throw an error"){
+                CHECK_THROWS(Square(64, board));
+            }
+        }
+        WHEN("You create the Square with an index too small"){
+            THEN("The Square should throw an error"){
+                CHECK_THROWS(Square(-1, board));                
+            }
+        }
+        WHEN("You create the Square with a string too long or wrong notation"){
+            THEN("The Square should throw an error"){
+                CHECK_THROWS(Square::fromNotation("abc", board));
+                CHECK_THROWS(Square::fromNotation("A9", board));
+                CHECK_THROWS(Square::fromNotation("I1", board));
+            }
+        }
+    }
+
 }
 
 TEST_CASE("Create Square from notation", "[square]"){
-    Square square = Square::fromNotation("A5"); //Create square with idx 35
+    Board board;
+    SECTION("A1"){
+        Square square = Square::fromNotation("A1", board);
 
-    CHECK(square.index() == 4);
-    CHECK(square.bit() == 0b10000);
-    CHECK(square.notation() == "A5");
+        CHECK(square.index() == 0);
+        CHECK(square.bit() == 0b1);
+        CHECK(square.notation() == "A1");
+    }
+
+    SECTION("A8"){
+        Square square = Square::fromNotation("A8", board);
+
+        CHECK(square.index() == 7);
+        CHECK(square.bit() == 0x80);
+        CHECK(square.notation() == "A8");
+    }
+
+    SECTION("H1"){
+        Square square = Square::fromNotation("H1", board);
+
+        CHECK(square.index() == 56);
+        CHECK(square.bit() == 0x100000000000000);
+        CHECK(square.notation() == "H1");
+    }
+
+    SECTION("H8"){
+        Square square = Square::fromNotation("H8", board);
+
+        CHECK(square.index() == 63);
+        CHECK(square.bit() == 0x8000000000000000);
+        CHECK(square.notation() == "H8");
+    }
+}
+
+TEST_CASE("Add Figure on Square", "[square]"){
+    GIVEN("A Square on A3 and a Figure on A4"){
+        Board board;
+        Square square = Square::fromNotation("A3", board);
+        auto figure = Figure::fromNotation(Color::Black, "A4", board);
+        auto figptr = figure.get();
+        
+
+        WHEN("You add the Figure to the Square and to the Board"){
+            square.set_figure(figptr);
+            board.addFigure(std::move(figure), square.index());
+            
+            THEN("The Figure should be accessible by the Square"){
+                CHECK(square.get_figure() == figptr);
+            }
+            THEN("The Figure square index should change to the Square"){
+                CHECK(figptr->get_square().index() == square.index());
+            }
+            THEN("The change should also show in the Board Square"){
+                CHECK(board.get_figure_on(square.index()) == figptr);
+            }
+        }
+    }
 }

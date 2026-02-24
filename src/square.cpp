ce86ff004df03dbd0ce9ce711b20adc2f515d05a
Marian <marian.otto@gmx.net>
2026-02-24 21:28:33 +0100

    Fix Adjust Parameters Add Exceptions Add get_figure()

--- src/square.cpp
+++ src/square.cpp
@@ -1,14 +1,16 @@
 #include "../include/square.h"
+#include "../include/figure.h"
 #include <stdexcept>
 
-Square::Square(Index idx)
-:_index(idx){
+Square::Square(Index idx, Board& board)
+:_index(idx), _board(board){
+    _figure = nullptr;
     if (idx >= 64) {
         throw std::out_of_range("Square index must be 0..63");
     }
 }
 
-Square Square::fromNotation(std::string notation){
+Square Square::fromNotation(std::string notation, Board& board){
     if(notation.size() != 2){
         std::length_error("Invalid notation size");
     }
@@ -16,18 +18,16 @@ Square Square::fromNotation(std::string notation){
     char file = notation[0];
     char rank = notation[1];
 
-    if (!(('a' <= file && file <= 'h') || (file >= 'A' && file <= 'H'))) {
-        std::out_of_range("fromNotation[0] not between a..h or A..H");
-        return -1;
+    if (!(file >= 'A' && file <= 'H')) {
+        throw std::out_of_range("fromNotation[0] not between A..H");
     } else if (!('1' <= rank && rank <= '8')) {
-        std::out_of_range("fromNotation[1] not between 1..8");
-        return -1;
+        throw std::out_of_range("fromNotation[1] not between 1..8");
     }
 
-    file = ('a' <= file && file <= 'h') ? file - 'a' : file - 'A';
+    file = file - 'A';
     rank = rank - '1';
 
-    return Square((file) * 8 + rank);
+    return Square((file) * 8 + rank, board);
 }
 
 Square::Index Square::index() const{
@@ -43,4 +43,13 @@ std::string Square::notation() const {
     char file = 'A' + (_index / 8);
     char rank = '1' + (_index % 8);
     return {file, rank};
+}
+
+Figure* Square::get_figure(){
+    return _figure;
+}
+
+void Square::set_figure(Figure* figure){
+    figure->set_square_index(this->index());
+    _figure = figure;
 }

90dde9b2704bcce646bf8653429a13199e992519
Marian <marian.otto@gmx.net>
2026-02-24 21:27:07 +0100

    Add a referance to the board and a figure on the square

--- include/square.h
+++ include/square.h
@@ -1,21 +1,31 @@
 #pragma once
 
 #include <cstdint>
+#include <memory>
 #include <string>
+
+class Figure;
+class Board;
 
 class Square{
     public:
         using Index = uint8_t;
         using Bitmap = uint64_t;
     
-        Square(Index idx);
+        Square(Index idx, Board &board);
     
-        static Square fromNotation(std::string notation);
+        static Square fromNotation(std::string notation, Board &board);
     
         Index index() const;
         Bitmap bit() const;
         std::string notation() const;
+
+        Figure* get_figure();
+
+        void set_figure(Figure* figure);
     
     private:
         const Index _index;
+        Board& _board;
+        Figure* _figure;
     };

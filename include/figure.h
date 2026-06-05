dc92f2e86315ddf8e681e319202f3903ac827a33
Marian <marian.otto@gmx.net>
2026-06-05 23:50:49 +0200

    Enforce Type for fromNotation and add move
    Delete fromNotation varient without type specifyer to enforce type
    Add move and get_valid_squares to prepare for movement

--- include/figure.h
+++ include/figure.h
@@ -1,32 +1,40 @@
 #pragma once
-#include "../include/square.h"
 #include "../include/color.h"
+#include "../include/square.h"
 #include "../include/type.h"
 #include <memory>
+#include <vector>
 
 class Board;
 
-class Figure{
-    public:
-        Color get_color() const;
-        Square* get_square() const;
-        Type get_type() const;
-        bool get_has_moved() const;
-        Color get_opposite_color() const;
-        Board* get_board() const;
+class Figure
+{
+public:
+  Figure(Color color, Square* square, Type type, Board& board);
+  static std::unique_ptr<Figure> fromNotation(Color color,
+                                              std::string notation,
+                                              Type type,
+                                              Board& board);
 
-        void set_square(Square* square);
-        void set_has_moved(bool has_moved);
-        void set_type(Type type);
+  Color get_color() const;
+  Square* get_square() const;
+  Type get_type() const;
+  bool get_has_moved() const;
+  Color get_opposite_color() const;
+  Board* get_board() const;
 
-        Figure(Color color, Square* square, Type type, Board& board);
-        static std::unique_ptr<Figure> fromNotation(Color color, std::string notation, Board& board);
-        static std::unique_ptr<Figure> fromNotation(Color color, std::string notation, Type type, Board& board);
-    private:
-        Color _color;
-        Square* _square;
-        Type _type;
-        bool _has_moved;
-        Color _opposite_color;
-        Board& _board;
+  void set_square(Square* square);
+  void set_has_moved(bool has_moved);
+  void set_type(Type type);
+
+  virtual void move() = 0;
+  virtual std::vector<Square*> get_possible_moves() = 0;
+
+protected:
+  Color _color;
+  Square* _square;
+  Type _type;
+  bool _has_moved;
+  Color _opposite_color;
+  Board& _board;
 };

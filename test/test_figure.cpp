4f2307d7103ac660b1bef252ad0fea1eff9d7aa8
Marian <marian.otto@gmx.net>
2026-06-06 00:17:59 +0200

    Shorten Tests by splitting test up

--- test/test_figure.cpp
+++ test/test_figure.cpp
@@ -1,54 +1,81 @@
-#include "../include/catch.hpp"
-#include "../include/figure.h"
+#include "../include/bishop.h"
 #include "../include/board.h"
+#include "../include/catch.hpp"
+#include "../include/king.h"
+#include "../include/knight.h"
+#include "../include/pawn.h"
+#include "../include/queen.h"
+#include "../include/rook.h"
+#include "../include/testFigure.h"
 
-TEST_CASE("Create Figure", "[figure]"){
-    GIVEN("An empty board"){
-        Board board;
+TEST_CASE("Create Figure", "[figure]")
+{
+  Board board;
 
-        WHEN("You construct a white Figure on Square 8"){
-            Figure figure(Color::White, board.get_square_at(8), Type::None, board);
+  WHEN("You construct a white Figure on Square 8")
+  {
+    TestFigure figure(Color::White, board.get_square_at(8), board);
 
-            THEN("All the members should be set"){
-                CHECK(figure.get_color() == Color::White);
-                CHECK(figure.get_square() == board.get_square_at(8));
-                CHECK(figure.get_has_moved() == 0);
-                CHECK(figure.get_opposite_color() == Color::Black);
-                CHECK(figure.get_type() == Type::None);
-            }
-        }
-
-        WHEN("You construct a Figure with an invalid color"){
-            THEN("An exception should be thrown"){
-                CHECK_THROWS(Figure(Color::Any, board.get_square_at(8), Type::None, board));
-            }
-        }
+    THEN("All the members should be set")
+    {
+      CHECK(figure.get_color() == Color::White);
+      CHECK(figure.get_square() == board.get_square_at(8));
+      CHECK(figure.get_has_moved() == 0);
+      CHECK(figure.get_opposite_color() == Color::Black);
+      CHECK(figure.get_type() == Type::None);
     }
+  }
 
+  WHEN("You construct a Figure with an invalid color")
+  {
+    THEN("An exception should be thrown")
+    {
+      CHECK_THROWS(TestFigure(Color::Any, board.get_square_at(8), board));
+    }
+  }
 }
 
-TEST_CASE("Color should match opposite color", "[figure]"){
-    GIVEN("An empty Board"){
-        Board board;
+TEST_CASE("White has opposite color black", "[figure]")
+{
+  Board board;
+  TestFigure figure(Color::Black, board.get_square_at("H3"), board);
 
-        WHEN("You construct a black Figure"){
-            Figure figure(Color::Black, board.get_square_at("H3"), Type::None, board);
+  CHECK(figure.get_color() == Color::Black);
+  CHECK(figure.get_opposite_color() == Color::White);
+}
 
-            THEN("The colors should be opposite"){
-                CHECK(figure.get_color() == Color::Black);
-                CHECK(figure.get_opposite_color() == Color::White);
-            }
-        }
+TEST_CASE("You construct a white Figure", "[figure]")
+{
+  Board board;
+  TestFigure figure(Color::White, board.get_square_at("H3"), board);
 
-        WHEN("You construct a white Figure"){
-            Figure figure(Color::White, board.get_square_at("H3"), Type::None, board);
+  CHECK(figure.get_color() == Color::White);
+  CHECK(figure.get_opposite_color() == Color::Black);
+}
 
-            THEN("The colors should be opposite"){
-                CHECK(figure.get_color() == Color::White);
-                CHECK(figure.get_opposite_color() == Color::Black);
-            }
-        }
+TEST_CASE("fromNotation: Overloads behave consistantly", "[figure]")
+{
+  Board board;
+
+  WHEN("You create a Figure from Notation")
+  {
+    auto figure = TestFigure::fromNotation(Color::Black, "B1", board);
+
+    THEN("The square of the figure should be add to the square")
+    {
+      CHECK(figure->get_square() == board.get_square_at("B1"));
     }
+  }
+
+  WHEN("You create a Figure from Notation and specify the type")
+  {
+    auto figure = Figure::fromNotation(Color::Black, "C6", Type::Knight, board);
+
+    THEN("The type should be set to the given type")
+    {
+      CHECK(figure->get_type() == Type::Knight);
+    }
+  }
 }
 
 TEST_CASE("fromNotation: Create different Classes from Types", "[figure]")
@@ -80,27 +107,31 @@ TEST_CASE("fromNotation: Create different Classes from Types", "[figure]")
   }
 }
 
-TEST_CASE("Test setters", "[figure]"){
-    GIVEN("A white figure on C3"){
-        Board board;
-        auto figure = Figure::fromNotation(Color::White, "C3", board);
+TEST_CASE("Test setters", "[figure]")
+{
+  GIVEN("A white figure on C3")
+  {
+    Board board;
+    auto figure = TestFigure::fromNotation(Color::White, "C3", board);
 
-        REQUIRE(figure->get_square() == board.get_square_at("C3"));
-        REQUIRE(figure->get_has_moved() == 0);
-        REQUIRE(figure->get_type() == Type::None);
+    REQUIRE(figure->get_square() == board.get_square_at("C3"));
+    REQUIRE(figure->get_has_moved() == 0);
+    REQUIRE(figure->get_type() == Type::None);
 
-        WHEN("You set different members"){
-            figure->set_square(board.get_square_at(31));
-            figure->set_has_moved(1);
-            figure->set_type(Type::King);
+    WHEN("You set different members")
+    {
+      figure->set_square(board.get_square_at(31));
+      figure->set_has_moved(1);
+      figure->set_type(Type::King);
 
-            THEN("The all the members should change"){
-                CHECK(figure->get_square() == board.get_square_at(31));
-                CHECK(figure->get_has_moved() == 1);
-                CHECK(figure->get_type() == Type::King);
-            }
-        }
+      THEN("The all the members should change")
+      {
+        CHECK(figure->get_square() == board.get_square_at(31));
+        CHECK(figure->get_has_moved() == 1);
+        CHECK(figure->get_type() == Type::King);
+      }
     }
+  }
 }
 
 // TEST_CASE("Move Figure", "[figure]"){
@@ -126,10 +157,10 @@ TEST_CASE("Test setters", "[figure]"){
 
 //     /*
 //     TODO: Check for swap
-//     i.e. if figure moves to empty square or captures 
+//     i.e. if figure moves to empty square or captures
 //     piece the old square should be empty now
 //     */
 
 // }
-//TODO: move
-//TODO: get squares
+// TODO: move
+// TODO: get squares
